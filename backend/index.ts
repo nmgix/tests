be6d5d786d9cba8942630ce1e1db4ed05f9001da
Nmgix <frostdraky@gmail.com>
2022-11-21 23:01:38 +0300

    Added docker-compose and dockerfiles

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import cookies from "cookie-parser";
+import helmet from "helmet";
+
+import { AuthRouter } from "./routes/auth";
+import { TodoRouter } from "./routes/todo";
 
 const app = express();
 
+// app settings
+app.use(cookies());
+app.use(helmet());
+dotenv.config();
+
+// routes
+app.use("/auth", AuthRouter);
+app.use("/todo", TodoRouter);
+
+// access settings
 const port = process.env.PORT ?? 8080;
 app.listen(port, () => console.log(`Backend active on port ${port}`));
