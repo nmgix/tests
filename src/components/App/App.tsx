7d8504ea5fd065e7f24ff9aa5271a01b50ea539b
Nmgix <frostdraky@gmail.com>
2023-03-07 15:46:34 +0300

    Настроил Tailwind, почистил README.md

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
-import "./index.css";
-
 const App: React.FC = () => {
-  return <div className='App'>wow</div>;
+  return (
+    <div className='bg-gray-200 flex w-[695px]'>
+      <input
+        className='bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10'
+        type='email'
+        placeholder='jane@example.com'
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // iris/100
+        background: "#F3F4F6",
+        outline: "#E2E3E5",
+        // sky/50
+      },
+    },
+  },
+  plugins: [],
+};
