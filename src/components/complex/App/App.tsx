cec7ded29c8b19648e01fa302a897550027fc3cf
Nmgix <frostdraky@gmail.com>
2023-04-29 19:59:30 +0300

    Базовая версия пк, пока не pixel-perfect

--- src/components/complex/App/App.tsx
+++ src/components/complex/App/App.tsx
@@ -1,12 +1,14 @@
 import Background from "../../basic/Background";
 import Footer from "../Footer";
 import Header from "../Header";
+import HeroScreen from "../Screens/Hero";
 import classnames from "./app.module.scss";
 
 const App: React.FC = () => {
   return (
     <div className={classnames.app}>
       <Header />
+      <HeroScreen />
       <Footer />
       <Background />
     </div>
