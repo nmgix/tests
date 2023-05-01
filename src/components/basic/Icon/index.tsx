b8a114b6daffe4c61c9077e8885938be3df2fb46
Nmgix <frostdraky@gmail.com>
2023-05-01 18:56:07 +0300

    Фикс изображений на localhost

--- src/components/basic/Icon/index.tsx
+++ src/components/basic/Icon/index.tsx
@@ -15,7 +15,7 @@ type IconProps = {
   noAspectRatio?: true;
 };
 
-const route = process.env.NODE_ENV === "production" ? "/amoCRM-front-laylout" : "";
+const route = process.env.NODE_ENV === "production" ? "/amoCRM-front-laylout" : "/amocrm-front-laylout";
 
 const Icon: React.FC<IconProps> = ({ icon, width, height, noAspectRatio }) => {
   return <i className={"icon"} style={{ width, height, WebkitMask: `url(${route}/svg/${icon}.svg) no-repeat 50% 50%`, WebkitMaskSize: "contain", aspectRatio: !noAspectRatio ? "1/1" : "auto" }} />;
