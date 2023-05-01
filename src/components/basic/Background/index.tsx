28adc6364669f7a4e1bc7a89d8b39fe62ef4f7ed
Nmgix <frostdraky@gmail.com>
2023-05-01 18:31:32 +0300

    Пофиксил подгрузку изображений в деплое

--- src/components/basic/Background/index.tsx
+++ src/components/basic/Background/index.tsx
@@ -1,5 +1,7 @@
 import classnames from "./background.module.scss";
 
+const route = process.env.NODE_ENV === "production" ? "amoCRM-front-laylout" : "";
+
 const Background: React.FC = () => {
   return (
     <div className={classnames.wrapper}>
@@ -14,7 +16,7 @@ const Background: React.FC = () => {
         <div className={classnames.mobilePurpleLight} />
         <div className={classnames.mobileRedLight} />
         <div className={classnames.mobileYellowLight} />
-        <img className={classnames.mobileNoise} src={`/png/noise.png`} alt='background' draggable={false} />
+        <img className={classnames.mobileNoise} src={`${route}/png/noise.png`} alt='background' draggable={false} />
         <div className={classnames.mobileGlass} />
         <div className={classnames.mobileRedBall1} />
         <div className={classnames.mobileYellowBall1} />
--- src/components/basic/Icon/index.tsx
+++ src/components/basic/Icon/index.tsx
@@ -15,8 +15,10 @@ type IconProps = {
   noAspectRatio?: true;
 };
 
+const route = process.env.NODE_ENV === "production" ? "amoCRM-front-laylout" : "";
+
 const Icon: React.FC<IconProps> = ({ icon, width, height, noAspectRatio }) => {
-  return <i className={"icon"} style={{ width, height, WebkitMask: `url(/svg/${icon}.svg) no-repeat 50% 50%`, WebkitMaskSize: "contain", aspectRatio: !noAspectRatio ? "1/1" : "auto" }} />;
+  return <i className={"icon"} style={{ width, height, WebkitMask: `url(/${route}/svg/${icon}.svg) no-repeat 50% 50%`, WebkitMaskSize: "contain", aspectRatio: !noAspectRatio ? "1/1" : "auto" }} />;
 };
 
 export default Icon;
