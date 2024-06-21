06b7d79e52ab199a8f49d5c5139643b604afc36b
Nmgix <frostdraky@gmail.com>
2024-06-21 18:16:13 +0300

    fonts added

---
+++ src/shared/ui/icon/icon.tsx
@@ -0,0 +1,30 @@
+import classnames from "classnames";
+import { memo } from "react";
+
+enum AvailableIcons {}
+
+interface IIconProps {
+  /**
+   * Choose between available icons located at public/svg
+   */
+  icon: keyof typeof AvailableIcons;
+  /**
+   * background color for icon, set in `element.style`
+   * @optional
+   */
+  color?: string;
+  classNames?: string | string[];
+}
+
+export const Icon: React.FC<IIconProps> = ({ icon, color, classNames }) => {
+  return (
+    <svg className={classnames("icon", classNames)} style={{ color }}>
+      {/* <title>??? */}
+      <use xlinkHref={`/icons/svg/index.svg#${icon}`}></use>
+    </svg>
+  );
+};
+Icon.displayName = "Icon";
+
+export const IconMemo = memo(Icon, (prev, next) => prev.icon === next.icon && prev.color === next.color);
+IconMemo.displayName = "IconMemo";
